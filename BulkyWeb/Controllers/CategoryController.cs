bb998e991802f601ce2a4ad5a5518be8eba55461
mananqayas <mananqayas960@gmail.com>
2024-12-11 16:51:45 -0500

    first commit

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController(ApplicationDbContext db) : Controller
+{
+
+
+
+    // GET
+    public IActionResult Index()
+    {
+
+        List<Category> objCategoryList = db.Categories.ToList();
+        return View(objCategoryList);
+    }
+}
